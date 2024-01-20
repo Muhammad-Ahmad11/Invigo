2379b6bb89f00e3e387855f9ab713763db4ceeee
Muhammad99Ahmad99 <muhammadahmad788454@gmail.com>
2024-01-20 20:29:52 +0500

    Project Secuirty Updated

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-//const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
-const uri  = "mongodb+srv://muhammadahmad788454:ZA7NO1Ro7iG8myM4@cluster0.ot4hjob.mongodb.net/invigo_WP?retryWrites=true&w=majority"
 
 function main() {
     mongoose.connect(uri, {
